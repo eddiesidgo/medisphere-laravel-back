9416dbf3cfaa7740c4de81e68341c58577db04bd
Joshua Saib <96671709+joss68@users.noreply.github.com>
2025-04-28 19:41:31 -0600

    Update FE.php

--- app/Models/FE.php
+++ app/Models/FE.php
@@ -1,1 +1,30 @@
-//example 
+<?php
+
+namespace App\Models;
+
+use App\Models\FE;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Examen extends Model
+{
+    use HasFactory;
+
+    protected $table = 'examenes';
+
+    protected $fillable = [
+     
+    'Valor',
+    'precio',
+    'unidad_equi',
+    'iva',
+    'afecta'
+
+    ];
+
+   
+    public function show_FE()
+    {
+        return $this->belongsTo(FE::class);
+    }
+}
